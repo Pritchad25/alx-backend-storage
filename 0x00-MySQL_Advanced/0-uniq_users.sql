6d0dcdc0723e4e6cdd99e4500635176188e0b57e
Pritchad25 <pricts29@gmail.com>
2024-01-18 21:09:14 +0000

    fixing the indentation error in code.

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,8 +1,7 @@
 -- Creates a table called users on any database
---uses given atributes to do so
-
+-- uses given atributes to do so
 CREATE TABLE IF NOT EXISTS users (
-       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-       email varchar(255) NOT NULL UNIQUE,
-       name varchar(255)
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	email varchar(255) NOT NULL UNIQUE,
+	name varchar(255)
 )
