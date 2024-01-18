a967ad94e96c17707f302e54dfd63da8fa020e55
Pritchad25 <pricts29@gmail.com>
2024-01-18 20:52:24 +0000

    creates a table called users on any databse.

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,8 @@
+-- Creates a table called users on any database
+--uses given atributes to do so
+
+CREATE TABLE IF NOT EXISTS users (
+       id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+       email varchar(255) NOT NULL UNIQUE,
+       name varchar(255)
+)
